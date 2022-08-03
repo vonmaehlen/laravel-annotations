ccb4d34fde70b2fb2164e7da81b23bcb79c82d0b
Vincent Neubauer <v.neubauer@vonmaehlen.com>
2022-08-03 15:35:39 +0200

    Fix route attribute scanning
    The scanner would return all attributes and not just the ones, that were
    related to routing. But the following services depent on given
    attributes being routing attributes.

--- src/Routing/Attributes/AttributeStrategy.php
+++ src/Routing/Attributes/AttributeStrategy.php
@@ -2,6 +2,8 @@
 
 namespace Collective\Annotations\Routing\Attributes;
 
+use Collective\Annotations\Routing\Attributes\Attributes\Route;
+use Collective\Annotations\Routing\Meta;
 use Collective\Annotations\Routing\ScanStrategyInterface;
 use ReflectionAttribute;
 use ReflectionClass;
@@ -13,9 +15,14 @@ class AttributeStrategy implements ScanStrategyInterface
      */
     public function getClassMetaList(ReflectionClass $class): array
     {
+        $attributes = array_values(array_filter(
+            $class->getAttributes(),
+            fn($attribute) => $this->isRoutingAttribute($attribute->getName()),
+        ));
+
         return array_map(
             fn (ReflectionAttribute $attribute) => $attribute->newInstance(),
-            $class->getAttributes()
+            $attributes,
         );
     }
 
@@ -28,7 +35,10 @@ class AttributeStrategy implements ScanStrategyInterface
 
         foreach ($class->getMethods() as $method) {
             if ($method->class == $class->name) {
-                $results = $method->getAttributes();
+                $results = array_values(array_filter(
+                    $method->getAttributes(),
+                    fn($attribute) => $this->isRoutingAttribute($attribute->getName()),
+                ));
 
                 if (count($results) > 0) {
                     $attributes[$method->name] = array_map(
@@ -40,5 +50,10 @@ class AttributeStrategy implements ScanStrategyInterface
         }
 
         return $attributes;
+    }
+
+    protected function isRoutingAttribute(mixed $object_or_class): bool
+    {
+        return is_a($object_or_class, Meta::class, true);
     }
 }
